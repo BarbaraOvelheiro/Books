8d8c4e40079efe84dfac1910215078b347d99be7
Bárbara Ovelheiro <bovelheiro@hotmail.com>
2018-05-21 11:27:35 +0100

    Abrir a base de  dados

---
+++ app/src/main/java/pt/ipg/books/BooksDbOpenHelper.java
@@ -0,0 +1,26 @@
+package pt.ipg.books;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/* ABRIR A BASE DE DADOS */
+public class BooksDbOpenHelper extends SQLiteOpenHelper {
+
+    public static final int VERSION = 1;
+    public static final String DATABASE_NAME = "books.db";
+
+    public BooksDbOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
